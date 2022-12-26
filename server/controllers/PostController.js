995d1f56691e33a9c0a0f0ed1815bce70a5b163a
Elena <15el.shev@gmail.com>
2022-12-26 21:01:13 +0200

    Fixed PostController

--- server/controllers/PostController.js
+++ server/controllers/PostController.js
@@ -4,7 +4,7 @@ import CommentModel from "../models/Comment.js";
 
 export const getLastTags = async (req, res) => {
   try {
-    const posts = await PostModel.find().limit(5).exec();
+    const posts = await PostModel.find().sort("-updatedAt").limit(5).exec();
 
     const tags = posts
       .map((obj) => obj.tags)
